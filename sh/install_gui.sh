1ef650d874558f85b9055faa42c5186505d3f054
Mark Glasgow <markglasgow@gmail.com>
2022-03-12 17:52:45 +0000

    updates

--- sh/install_gui.sh
+++ sh/install_gui.sh
@@ -6,18 +6,28 @@
 # Run this with
 # bash -c "$(curl -s https://node.phenotype.dev)"
 
-# 1. Set some environment variables
+
+# / 
+# Improvements
+# / 
+# 1. Better error logging + bug detection
+# 2. Test-net options
+# 3. Check for node mistakenly thinking it's sync'ed (headerChainDiff?)
+# 4. Light-mode Yes/No
+
+# Set some environment variables
 set_environment(){
     export API_KEY="dummy"
     export BLAKE_HASH="unset"
 
+    let j=0
     #OS=$(uname -m)
 
     [ -d ergo ] || mkdir ergo && cd ergo
     
     
     dt=$(date '+%d/%m/%Y %H:%M:%S');
-    i=0
+    let i=0
     let PERCENT_BLOCKS=100
     let PERCENT_HEADERS=100
 
@@ -99,71 +109,71 @@ set_environment(){
 }
 
 set_configuration (){
-    echo "
-ergo {
-    node {
-        # Full options available at 
-        # https://github.com/ergoplatform/ergo/blob/master/src/main/resources/application.conf
-        
-        mining = false
-
-        # Number of state snapshot diffs to keep. Defines maximum rollback depth
-        #keepVersions = 32
-
-        ### there's light regime where the node is not storing UTXO set, and can validate only limited in length suffix of full blocks . Such nodes are running on Raspberry Pi with 0.5 GB given even.
-        # Skip validation of transactions in the mainnet before block 417,792 (in v1 blocks).
-        # Block 417,792 is checkpointed by the protocol (so its UTXO set as well).
-        # The node still applying transactions to UTXO set and so checks UTXO set digests for each block.
-        skipV1TransactionsValidation = true
-        
-        # Number of last blocks to keep with transactions and ADproofs, for all other blocks only header will be stored.
-        # Keep all blocks from genesis if negative
-        # download and keep only ~4 days of full-blocks
-        #$blocksToKeep
-        
-        # A node is considering that the chain is synced if sees a block header with timestamp no more
-        # than headerChainDiff blocks on average from future
-        # testnet value is 800 blocks ~= 1600 minutes (~1.1 days)
-        #headerChainDiff = 800
-
-        # State type.  Possible options are:
-        # "utxo" - keep full utxo set, that allows to validate arbitrary block and generate ADProofs
-        # "digest" - keep state root hash only and validate transactions via ADProofs
-        $stateType
-
-
-    }
-
-}      
-        
-scorex {
-    restApi {
-        # Hex-encoded Blake2b256 hash of an API key. 
-        # Should be 64-chars long Base16 string.
-        # below is the hash of the string 'hello'
-        # replace with your actual hash 
-        apiKeyHash = "$BLAKE_HASH"
-        
-    }
-    network {
+        echo "
+    ergo {
+        node {
+            # Full options available at 
+            # https://github.com/ergoplatform/ergo/blob/master/src/main/resources/application.conf
             
-            # Misbehaving peer penalty score will not be increased withing this time interval,
-            # unless permanent penalty is applied
-            #penaltySafeInterval = 1m
+            mining = false
+
+            # Number of state snapshot diffs to keep. Defines maximum rollback depth
+            #keepVersions = 32
+
+            ### there's light regime where the node is not storing UTXO set, and can validate only limited in length suffix of full blocks . Such nodes are running on Raspberry Pi with 0.5 GB given even.
+            # Skip validation of transactions in the mainnet before block 417,792 (in v1 blocks).
+            # Block 417,792 is checkpointed by the protocol (so its UTXO set as well).
+            # The node still applying transactions to UTXO set and so checks UTXO set digests for each block.
+            skipV1TransactionsValidation = true
             
-            # Max penalty score peer can accumulate before being banned
-            #penaltyScoreThreshold = 100
+            # Number of last blocks to keep with transactions and ADproofs, for all other blocks only header will be stored.
+            # Keep all blocks from genesis if negative
+            # download and keep only ~4 days of full-blocks
+            #$blocksToKeep
+            
+            # A node is considering that the chain is synced if sees a block header with timestamp no more
+            # than headerChainDiff blocks on average from future
+            # 800 blocks ~= 1600 minutes (~1.1 days)
+            headerChainDiff = 80
 
-            # Max number of delivery checks. Stop expecting modifier (and penalize peer) if it was not delivered after that
-            # number of delivery attempts
-            #maxDeliveryChecks = 2
+            # State type.  Possible options are:
+            # "utxo" - keep full utxo set, that allows to validate arbitrary block and generate ADProofs
+            # "digest" - keep state root hash only and validate transactions via ADProofs
+            $stateType
 
-               
-            maxConnections = 10
 
         }
-}
-    " > ergo.conf
+
+    }      
+            
+    scorex {
+        restApi {
+            # Hex-encoded Blake2b256 hash of an API key. 
+            # Should be 64-chars long Base16 string.
+            # below is the hash of the string 'hello'
+            # replace with your actual hash 
+            apiKeyHash = "$BLAKE_HASH"
+            
+        }
+        network {
+                
+                # Misbehaving peer penalty score will not be increased withing this time interval,
+                # unless permanent penalty is applied
+                #penaltySafeInterval = 1m
+                
+                # Max penalty score peer can accumulate before being banned
+                #penaltyScoreThreshold = 100
+
+                # Max number of delivery checks. Stop expecting modifier (and penalize peer) if it was not delivered after that
+                # number of delivery attempts
+                #maxDeliveryChecks = 2
+
+                
+                maxConnections = 100
+
+            }
+    }
+        " > ergo.conf
 
 }
 
@@ -175,58 +185,56 @@ start_node(){
     #error_log
 }
 
-
+# Set basic config for boot, boot & get the hash and then re-set config 
 first_run() {
-# Check for .log files to see if this is the first run
-# If(.log) -> extract env -> start_node
-# Set basic config for boot, boot & get the hash and then re-set config              
-         
-    ### Download the latest .jar file                                                                    
-    if [ ! -e *.jar ]; then 
-        echo "- Retrieving latest node release.."
-        LATEST_ERGO_RELEASE=$(curl -s "https://api.github.com/repos/ergoplatform/ergo/releases/latest" | awk -F '"' '/tag_name/{print $4}')
-        LATEST_ERGO_RELEASE_NUMBERS=$(echo ${LATEST_ERGO_RELEASE} | cut -c 2-)
-        ERGO_DOWNLOAD_URL=https://github.com/ergoplatform/ergo/releases/download/${LATEST_ERGO_RELEASE}/ergo-${LATEST_ERGO_RELEASE_NUMBERS}.jar
-        echo "- Downloading Latest known Ergo release: ${LATEST_ERGO_RELEASE}."
-        curl --silent -L ${ERGO_DOWNLOAD_URL} --output ergo.jar
-    fi 
 
-   
-    
-    # API 
-    read -p "
-#### Please create a password. #### 
-
-This will be used to unlock your API. 
-
-Generally using the same API key through the entire sync process can prevent 'Bad API Key' errors:
-
-" input
-
-    export API_KEY=$input
-    echo "$API_KEY" > api.conf
+            
+        ### Download the latest .jar file                                                                    
+        if [ ! -e *.jar ]; then 
+            echo "- Retrieving latest node release.."
+            LATEST_ERGO_RELEASE=$(curl -s "https://api.github.com/repos/ergoplatform/ergo/releases/latest" | awk -F '"' '/tag_name/{print $4}')
+            LATEST_ERGO_RELEASE_NUMBERS=$(echo ${LATEST_ERGO_RELEASE} | cut -c 2-)
+            ERGO_DOWNLOAD_URL=https://github.com/ergoplatform/ergo/releases/download/${LATEST_ERGO_RELEASE}/ergo-${LATEST_ERGO_RELEASE_NUMBERS}.jar
+            echo "- Downloading Latest known Ergo release: ${LATEST_ERGO_RELEASE}."
+            curl --silent -L ${ERGO_DOWNLOAD_URL} --output ergo.jar
+        fi 
 
     
-    
-    start_node
-    
-    export BLAKE_HASH=$(curl --silent -X POST "http://localhost:9053/utils/hash/blake2b" -H "accept: application/json" -H "Content-Type: application/json" -d "\"$input\"")
-    echo "$BLAKE_HASH" > blake.conf
-    #echo "BLAKE_HASH:$BLAKE_HASH"
-    
-    case_kill
+        
+        # API 
+        read -p "
+    #### Please create a password. #### 
 
-    # Add blake hash
-    set_configuration
-    
-    start_node
-    
-    # Add blake hash
-    #set_configuration
+    This will be used to unlock your API. 
+
+    Generally using the same API key through the entire sync process can prevent 'Bad API Key' errors:
+
+    " input
+
+        export API_KEY=$input
+        echo "$API_KEY" > api.conf
+
+        
+        
+        start_node
+        
+        export BLAKE_HASH=$(curl --silent -X POST "http://localhost:9053/utils/hash/blake2b" -H "accept: application/json" -H "Content-Type: application/json" -d "\"$input\"")
+        echo "$BLAKE_HASH" > blake.conf
+        #echo "BLAKE_HASH:$BLAKE_HASH"
+        
+        func_kill
+
+        # Add blake hash
+        set_configuration
+        
+        start_node
+        
+        # Add blake hash
+        #set_configuration
 
 }
 
-case_kill(){
+func_kill(){
     case "$(uname -s)" in
 
     CYGWIN*|MINGW32*|MSYS*|MINGW*)
@@ -270,7 +278,7 @@ error_log(){
     if [ $i -gt 10 ]; then
         i=0
         echo i: $i
-        #case_kill
+        #func_kill
         curl -X POST --max-time 10 "http://127.0.0.1:9053/node/shutdown" -H "api_key: $API_KEY"
         start_node
         
@@ -287,7 +295,7 @@ check_status(){
     
     if [ -z "$string" ]; then
         echo -e "${LRED}${1} is down${NC}"
-        case_kill
+        func_kill
         
         start_node
         print_console
@@ -353,25 +361,38 @@ get_heights(){
 
         if [ -n "$HEIGHT" ] && [ "$HEIGHT" -eq "$HEIGHT" ] 2>/dev/null; then
             let expr PERCENT_BLOCKS=$(( ( ($API_HEIGHT - $HEIGHT) * 100) / $API_HEIGHT   ))
-        fi
-        # None: integer expression expected when headers = None
-        #if [ -n "$FULL_HEIGHT" ] && [ "$FULL_HEIGHT" -eq "$HEIGHT" ] && [ "$HEADERS_HEIGHT" -eq "$API_HEIGHT" ] 2>/dev/null; then
-        #    echo "triggered"
-            #echo "ERROR: Height Mismatch. Failed at height $HEADERS_HEIGHT " >> error.log
-        #    sleep 10
-            #exit 1
-            #case_kill
-            #rm -rf .ergo/history
-            #rm -rf .ergo/state
-            #start_node
-        #fi
-
-
+        fi        
         
+        if [ -n "$h_tmp" ] && [ "$HEIGHT" -eq "$h_tmp" ]; then
+            echo "height changed"
+            echo "height:$HEIGHT, h_temp:$h_temp"
+            j=0
+            sleep 5
+        else 
+            echo "height same"
+            j=$((j+1))
+            echo "height:$HEIGHT==h_temp:$h_temp, j:$j"
+            sleep 5
+        fi
+        h_temp=$HEIGHT
 
+        # if height==headersHeight then we are syncronised. 
+        if [ -n "$HEADERS_HEIGHT" ] && [ "$HEADERS_HEIGHT" -eq "$HEIGHT" ] 2>/dev/null; then
+            echo "HeadersHeight == Height"
+            echo "Node is syncronised"
+            exit 1
 
-    else
-        echo "sync?"
+        fi
+
+        # If HeadersHeight < Height then something has gone wrong
+        if [ -n "$HEADERS_HEIGHT" ] && [ "$HEADERS_HEIGHT" -lt "$HEIGHT" ] 2>/dev/null; then
+            echo "HeadersHeight < Height"
+            echo "Mis-sync!"
+            exit 1
+
+        fi
+   
+        
 
     fi
     
@@ -406,15 +427,15 @@ print_console() {
 }
 
 
-######################
+# /
 # main()
-######################
+# / 
 
-# 1. Set some environment variables
+# Set some environment variables
 set_environment     
 
 # Cross-platform killer
-case_kill   
+func_kill   
 
 # Check for the prescence of log files
 count=`ls -1 blake.conf 2>/dev/null | wc -l`
